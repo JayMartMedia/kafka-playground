c03129b79eb0889a69a8f948581e94b2c3797002
JayMartMedia <Jay@JayMartMedia.com>
2023-06-22 23:15:12 -0400

    feat: add order notification title when dotnet consumer starting

--- dotnet-consumer/Program.cs
+++ dotnet-consumer/Program.cs
@@ -2,11 +2,20 @@
 
 class Program
 {
+    private static string title = @"
+   ____          _             _   _       _   _  __ _           _   _                _____                 _          
+  / __ \        | |           | \ | |     | | (_)/ _(_)         | | (_)              / ____|               (_)         
+ | |  | |_ __ __| | ___ _ __  |  \| | ___ | |_ _| |_ _  ___ __ _| |_ _  ___  _ __   | (___   ___ _ ____   ___  ___ ___ 
+ | |  | | '__/ _` |/ _ \ '__| | . ` |/ _ \| __| |  _| |/ __/ _` | __| |/ _ \| '_ \   \___ \ / _ \ '__\ \ / / |/ __/ _ \
+ | |__| | | | (_| |  __/ |    | |\  | (_) | |_| | | | | (_| (_| | |_| | (_) | | | |  ____) |  __/ |   \ V /| | (_|  __/
+  \____/|_|  \__,_|\___|_|    |_| \_|\___/ \__|_|_| |_|\___\__,_|\__|_|\___/|_| |_| |_____/ \___|_|    \_/ |_|\___\___|
+    ";
+
     public static void Main(string[] args)
     {
         var conf = new ConsumerConfig
         { 
-            GroupId = "dotnet-group2",
+            GroupId = "dotnet-group",
             BootstrapServers = "localhost:9092",
             // Note: The AutoOffsetReset property determines the start offset in the event
             // there are not yet any committed offsets for the consumer group for the
@@ -25,6 +34,8 @@ class Program
                 e.Cancel = true; // prevent the process from terminating.
                 cts.Cancel();
             };
+
+            Console.WriteLine(title);
 
             try
             {
